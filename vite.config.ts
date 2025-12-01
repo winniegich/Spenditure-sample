b0464e16d85d2726fb4dbe5b51bad55c71e67d13
winnie <winnienyaguthiigichuki.com>
2025-12-01 10:00:35 +0300

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
